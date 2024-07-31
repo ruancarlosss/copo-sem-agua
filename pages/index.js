0d1920921294a432ed094015ccab6ddad3f59ae4
Ruan Carlos De Souza <84088135+ruancarlosss@users.noreply.github.com>
2024-07-31 00:43:57 +0000

    Frase alterada

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>eu amo batata frita, com pouco sal</h1>
+    return <h1>No que você está pensando? Gosto de pessoas quietas e caladas...nunca se sabe se estão carregando o peso do mundo ou observando como ele age....</h1>
 }
 
 export default Home;
